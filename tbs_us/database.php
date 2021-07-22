66a1ffae9e4c998551db95195626b7cb5a70a0ad
unknown <nauval.hafidzi@gmail.com>
2021-07-22 23:52:36 +0700

    revisi sertifikat

--- tbs_us/database.php
+++ tbs_us/database.php
@@ -1,8 +1,8 @@
 <?php
 // Create connection
  //$con=mysqli_connect("198.38.82.92","earsip_admin","4dm1nsaja","earsip_kediri");
- $con=mysqli_connect("ls-faa4a2656edc82c3f2ac28f451e2d48ca317d20a.cpe3c4hmmuxu.us-east-2.rds.amazonaws.com","dbmasteruser","Jombang74","nuportal");
- // $con=mysqli_connect("localhost","root","","nuportal");
+ // $con=mysqli_connect("ls-faa4a2656edc82c3f2ac28f451e2d48ca317d20a.cpe3c4hmmuxu.us-east-2.rds.amazonaws.com","dbmasteruser","Jombang74","nuportal");
+ $con=mysqli_connect("localhost","root","","nuportal");
 
 // Check connection
  if (mysqli_connect_errno($con))
